4dc59972921568933d80dc5890c310aee363cbb9
Anar Abbasov <anar_abbasov@hotmail.com>
2023-05-26 22:56:33 -0700

    test

--- DRA818_transceiver.ino
+++ DRA818_transceiver.ino
@@ -1,34 +1,14 @@
 #include <stdio.h>
 #include <SoftwareSerial.h>
-#include "DRA818.h"
+
 
 #define PD      10  // to the DRA818 PD pin
 #define RX      3   // arduino serial RX pin to the DRA818 TX pin
 #define TX      4   // arduino serial TX pin to the DRA818 RX pin
 SoftwareSerial dra_serial(3,4); // Serial connection to DRA818
-//DRA818 *dra;                // the DRA object once instanciated
+              // the DRA object once instanciated
 float freq;                 // the next frequency to scan
 
-
-int  read_response() {
-  char ack[3];
-  ack[0] = ack[1] = ack[2] = '\0';  // Just to quiet some warnings.
-
-  
-  ack[2]=0;
-  long start = millis();
-  do {
-    if (dra_serial.available()) {
-      ack[0] = ack[1];
-      ack[1] = ack[2];
-      ack[2] =dra_serial.read();
-      Serial.println("reading");
-    }
-  } while (ack[2] != 0xa && (millis() - start) <   2000);
- 
-
-   return (ack[0] == '0');
-}
 
 
 void setup() {
@@ -37,33 +17,22 @@ void setup() {
    //pinMode( RX , INPUT);
     //pinMode(TX, OUTPUT);
   
-  Serial.print("initializing DRA818 ... ");
+  Serial.println("initializing DRA818 ... ");
   
   
   dra_serial.begin(9600);
   
-    Serial.print("sending... ");
-     char i = 3;
+    Serial.println("sending... ");
+  
+  dra_serial.write("AT+DMOCONNECT");
+   dra_serial.write("\r\n");
+ Serial.println(dra_serial.readString());
 
-
-  while (i-- > 0) {
-   
-   Serial.println("trying... ");
-  dra_serial.write("AT+DMOCONNECT\r\n");
- Serial.print(read_response());
-
-  }
- dra_serial.write("S+146.520\r\n"); 
- Serial.print(read_response());
-dra_serial.write("AT+VERSION\r\n"); 
- Serial.print(read_response());
+  
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
- // if (dra_serial.isListening()) { 
-  //      Serial.println("portOne is listening!");
-  //  }
+ 
 
 }
