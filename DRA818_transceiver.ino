90c363f4551f640d9490c8007d90dae3791e3137
Anar Abbasov <anar_abbasov@hotmail.com>
2023-05-27 10:46:40 -0700

    fixed uart

--- DRA818_transceiver.ino
+++ DRA818_transceiver.ino
@@ -12,6 +12,8 @@ float freq;                 // the next frequency to scan
 
 
 void setup() {
+  pinMode(PD, OUTPUT);                     // Power control of the DRA818
+  digitalWrite(PD,HIGH);  
   // put your setup code here, to run once:
    Serial.begin(9600); // for logging
    //pinMode( RX , INPUT);
@@ -23,13 +25,18 @@ void setup() {
   dra_serial.begin(9600);
   
     Serial.println("sending... ");
+  delay(3000);
+  dra_serial.println("AT+DMOCONNECT");
   
-  dra_serial.write("AT+DMOCONNECT");
-   dra_serial.write("\r\n");
  Serial.println(dra_serial.readString());
-
-  
+   dra_serial.println("S+146.520");
+ Serial.println(dra_serial.readString());
  
+   dra_serial.println("AT+SETTAIL=1");
+   Serial.println(dra_serial.readString());
+  dra_serial.println("AT+DMOSETGROUP=0,146.5200,146.5200,0,4, 0");
+   Serial.println(dra_serial.readString());
+    
 }
 
 void loop() {
